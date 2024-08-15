9797dfa23d84189db5fb68602cfa7332fe5adb5c
Aaron Nwabuoku <aaron@chatkitty.com>
2024-08-15 02:40:47 -0400

    fix: build functions on release

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import {auth, logger} from "firebase-functions";
 import {ChatKitty} from "chatkitty-platform-sdk";
 
 const chatkitty = new ChatKitty({
@@ -6,9 +6,8 @@ const chatkitty = new ChatKitty({
   clientSecret: process.env.CLIENT_SECRET as string,
 });
 
-
 export const handleCreateUser =
-    functions.auth.user().onCreate(async (user) => {
+    auth.user().onCreate(async (user) => {
       await chatkitty.Users.checkUserExists(user.uid).catch(async () => {
         await chatkitty.Users.createUser({
           name: user.uid,
@@ -16,14 +15,20 @@ export const handleCreateUser =
           isGuest: true,
         });
       });
+
+      logger.info(`Successfully created ChatKitty user: ${user.uid}`);
     });
 
-export const handleDeleteUser = functions.auth.user().onDelete(async (user) => {
+export const handleDeleteUser = auth.user().onDelete(async (user) => {
   const result = await chatkitty.Users.listUsers(0, 0, undefined, user.uid);
 
   const users = result.data._embedded?.users;
 
   if (users && users.length > 0) {
     await chatkitty.Users.deleteUser(users[0].id);
+
+    logger.info(`Successfully deleted ChatKitty user: ${user.uid}`);
+  } else {
+    logger.info(`ChatKitty user not found: ${user.uid}`);
   }
 });
